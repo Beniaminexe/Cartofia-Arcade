f0d0405300373e76706a0a225263f4011b2260be
Stefan-Beniamin Manolache <81258509+Beniaminexe@users.noreply.github.com>
2025-12-09 01:52:56 +0000

    Revert "Feature/bot foundation"

--- src/cartofia_bot/__init__.py
+++
@@ -1,7 +0,0 @@
-# src/cartofia_bot/commands/__init__.py
-"""
-Command modules for the Cartofia bot.
-
-Each module in this package should expose a `register(tree, config)` function
-that attaches its slash commands to the given CommandTree.
-"""
